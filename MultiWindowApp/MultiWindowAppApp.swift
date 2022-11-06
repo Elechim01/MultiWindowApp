4856eb06fa3157e394dabd83420765be43229a3c
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-11-06 18:19:23 +0100

    Initial Commit

---
+++ MultiWindowApp/MultiWindowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiWindowAppApp.swift
+//  MultiWindowApp
+//
+//  Created by Michele Manniello on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiWindowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
